e7b06b176fc8e816ddc6c6da8848e4c5678ca04f
Johnn <johnndavid27@gmail.com>
2025-04-11 00:02:09 -0300

    feat: configurações de segurança para deploy em HTTPS

--- app/settings.py
+++ app/settings.py
@@ -12,6 +12,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+SECURE_HSTS_SECONDS = 31536000  # 1 ano
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-brvyz!rd0yr3+lf(mh2v8y7i#4he#)#0)=b$&m%2=meoz*#%j$'
+SECRET_KEY = 'civg=bn(neyxel=sm(24l0^nlk01b-4_)hae-zhog7j9izmiia'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
